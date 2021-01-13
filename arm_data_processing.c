ff8e5dee760bbc9b6f6eefc2ee3ed4b87685b20b
Felix Vigne <vignef@im2ag-mandelbrot.e.ujf-grenoble.fr>
2021-01-13 18:45:39 +0100

    arm_data_processing.c qui compile

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -27,7 +27,7 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
-enum opcodes {AND,EOR,SUB,RSB,ADD,ADC,SBC,RSC,TST,TEQ,CMP,CMN,ORR,MOV,BIC,MVN}
+enum opcodes {AND,EOR,SUB,RSB,ADD,ADC,SBC,RSC,TST,TEQ,CMP,CMN,ORR,MOV,BIC,MVN};
 
 int borrowFrom(uint32_t op1, uint16_t op2){
     if(op1<op2)
@@ -35,19 +35,13 @@ int borrowFrom(uint32_t op1, uint16_t op2){
     return 0;          
 }
 
-int borrowFrom(uint16_t op1, uint32_t op2){
+int borrowFromRev(uint16_t op1, uint32_t op2){
     if(op1<op2)
       return 1;
     return 0;          
 }
 
 int carryFrom(uint32_t op1, uint16_t op2){
-    if((int)op1+(int)op2>0xFFFFFFFF)
-      return 1;
-    return 0;          
-}
-
-int carryFrom(uint16_t op1, uint32_t op2){
     if((int)op1+(int)op2>0xFFFFFFFF)
       return 1;
     return 0;          
@@ -65,7 +59,7 @@ int overflowFrom(uint32_t op1, uint16_t op2, uint32_t res, int isAddition){
     }        
 }
 
-int overflowFrom(uint16_t op1, uint32_t op2, uint32_t res, int isAddition){
+int overflowFromRev(uint16_t op1, uint32_t op2, uint32_t res, int isAddition){
     if(isAddition){
       if(get_bit(op1,11)==get_bit(op2,31) && get_bit(op1,11)!=get_bit(res,31))      
         return 1;
@@ -85,18 +79,21 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
     uint8_t rd = get_bits(ins,19,16);
     uint8_t rn = get_bits(ins,15,11);
     uint8_t S = get_bit(ins,20);
+    uint32_t rnVal = arm_read_register(p,rn);
+    uint16_t notcflag = !get_bit(arm_read_cpsr(p),29); 
+    uint16_t cflag = get_bit(arm_read_cpsr(p),29);     
 
-    switch (opcode)?{
+    switch (opcode){
 
         case AND:                  
           arm_write_register(p,rd,arm_read_register(p,rn)&shifter_operand); //AND
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x3FFFFFFF;        //on passse N Z et C à 0, on garde V et C
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;      //N flag = rd[31]
             if(arm_read_register(p,rd)==0)                                  //Z flag = 1 si rd=0 et 0 sinon
@@ -106,17 +103,18 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
             
             arm_write_cpsr(p,flags|clearflagscpsr);
           }
+          return NO_ERROR;
         break;
 
         case EOR:
           arm_write_register(p,rd,arm_read_register(p,rn)^shifter_operand);   //EOR
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x3FFFFFFF;        
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;  
             if(arm_read_register(p,rd)==0)                              
@@ -126,162 +124,163 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
             
             arm_write_cpsr(p,flags|clearflagscpsr);
           }
+          return NO_ERROR;
         break;
 
-        case SUB:
-          uint32_t rnVal=arm_read_register(p,rn);        
+        case SUB:;     
           arm_write_register(p,rd,rnVal-shifter_operand);                     //SUB
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x0FFFFFFF;              //cette fois on clear V aussi
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;                          //N
             if(arm_read_register(p,rd)==0)                              
               flags += 1;                                                                       //Z
             flags = flags<<1;
             
-            flags = (flags + ~(get_bit(borrowFrom(rnVal,shifter_operand),0))) << 1;             //C
+            flags = (flags + !get_bit(borrowFrom(rnVal,shifter_operand),0)) << 1;             //C
             
-            flags += get_bit(overflowFrom(rnVal,shifter_operand,arm_read_register(p,rd),0),0)); //V
+            flags += get_bit(overflowFrom(rnVal,shifter_operand,arm_read_register(p,rd),0),0); //V
             
             flags = flags<<28;                                                                  //shift de 28 cette fois pour V
             
             arm_write_cpsr(p,flags|clearflagscpsr);
           }
+          return NO_ERROR;
         break;
 
-        case RSB:
-          uint32_t rnVal=arm_read_register(p,rn);        
+        case RSB:;      
           arm_write_register(p,rd,shifter_operand-rnVal);                     //RSB
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x0FFFFFFF;              
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;                          //N
             if(arm_read_register(p,rd)==0)                              
               flags += 1;                                                                       //Z
             flags = flags<<1;
             
-            flags = (flags + ~(get_bit(borrowFrom(shifter_operand,rnVal),0))) << 1;             //C
+            flags = (flags + !(get_bit(borrowFromRev(shifter_operand,rnVal),0))) << 1;             //C
             
-            flags += get_bit(overflowFrom(shifter_operand,rnVal,arm_read_register(p,rd),0),0)); //V
+            flags += get_bit(overflowFromRev(shifter_operand,rnVal,arm_read_register(p,rd),0),0); //V
             
             flags = flags<<28;                                                                  
             
             arm_write_cpsr(p,flags|clearflagscpsr);
+          }
+          return NO_ERROR;
         break;
 
-        case ADD:
-          uint32_t rnVal=arm_read_register(p,rn);        
+        case ADD:;       
           arm_write_register(p,rd,rnVal+shifter_operand);                     //ADD
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x0FFFFFFF;              
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;                          //N
             if(arm_read_register(p,rd)==0)                              
               flags += 1;                                                                       //Z
             flags = flags<<1;
             
-            flags = (flags + get_bit(carryFrom(shifter_operand,rnVal),0)) << 1;              //C
+            flags = (flags + get_bit(carryFrom(rnVal,shifter_operand),0)) << 1;              //C
             
-            flags += get_bit(overflowFrom(shifter_operand,rnVal,arm_read_register(p,rd),1),0)); //V
+            flags += get_bit(overflowFrom(rnVal,shifter_operand,arm_read_register(p,rd),1),0); //V
             
             flags = flags<<28;                                                                  
             
             arm_write_cpsr(p,flags|clearflagscpsr);
+          }
+          return NO_ERROR;
         break;
 
-        case ADC:
-          uint32_t rnVal=arm_read_register(p,rn);
-          uint16_t cflag = get_bit(arm_read_cpsr(p),29);      
+        case ADC:;    
           arm_write_register(p,rd, rnVal + shifter_operand + cflag );                     //ADC
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x0FFFFFFF;              
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;                          //N
             if(arm_read_register(p,rd)==0)                              
               flags += 1;                                                                       //Z
             flags = flags<<1;
             
-            flags = (flags + get_bit(carryFrom(shifter_operand+cflag,rnVal),0)) << 1;              //C
+            flags = (flags + get_bit(carryFrom(rnVal,shifter_operand+cflag),0)) << 1;              //C
             
-            flags += get_bit(overflowFrom(shifter_operand+cflag,rnVal,arm_read_register(p,rd),1),0)); //V
+            flags += get_bit(overflowFrom(rnVal,shifter_operand+cflag,arm_read_register(p,rd),1),0); //V
             
             flags = flags<<28;                                                                  
             
             arm_write_cpsr(p,flags|clearflagscpsr);
+          }
+          return NO_ERROR;
         break;
 
-        case SBC:
-          uint32_t rnVal=arm_read_register(p,rn);
-          uint16_t notcflag = ~get_bit(arm_read_cpsr(p),29);           
+        case SBC:;       
           arm_write_register(p,rd,rnVal-shifter_operand-notcflag);                     //SBC
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
               //UNPREDICTABLE
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x0FFFFFFF;              
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;                          //N
             if(arm_read_register(p,rd)==0)                              
               flags += 1;                                                                       //Z
             flags = flags<<1;
             
-            flags = (flags + ~(get_bit(borrowFrom(rnVal,shifter_operand - notcflag),0))) << 1;             //C
+            flags = (flags + !(get_bit(borrowFrom(rnVal,shifter_operand - notcflag),0))) << 1;             //C
             
-            flags += get_bit(overflowFrom(rnVal,shifter_operand - notcflag,arm_read_register(p,rd),0),0)); //V
+            flags += get_bit(overflowFrom(rnVal,shifter_operand - notcflag,arm_read_register(p,rd),0),0); //V
             
             flags = flags<<28;                                                                  //shift de 28 
             
             arm_write_cpsr(p,flags|clearflagscpsr);
           }
+          return NO_ERROR;
         break;
         
-        case RSC:
-          uint32_t rnVal=arm_read_register(p,rn);
-          uint16_t notcflag = ~get_bit(arm_read_cpsr(p),29);           
+        case RSC:;       
           arm_write_register(p,rd,shifter_operand-rnVal-notcflag);                     //SBC
-          if (S==1 && rd==R15){
+          if (S==1 && rd==PC){
             if (arm_current_mode_has_spsr(p)){
               arm_write_cpsr(p,arm_read_spsr(p));
             }else{
-              //UNPREDICTABLE
+              //UNPREDICTABLE;
             }
-          }else if S==1{
+          }else if (S==1){
             uint32_t clearflagscpsr = arm_read_cpsr(p) & 0x0FFFFFFF;              
             uint32_t flags = get_bit(arm_read_register(p,rd),31) << 1;                          //N
             if(arm_read_register(p,rd)==0)                              
               flags += 1;                                                                       //Z
             flags = flags<<1;
             
-            flags = (flags + ~(get_bit(borrowFrom(shifter_operand - notcflag,rnVal),0))) << 1;             //C
+            flags = (flags + !(get_bit(borrowFromRev(shifter_operand - notcflag,rnVal),0))) << 1;             //C
             
-            flags += get_bit(overflowFrom(shifter_operand - notcflag,rnVal,arm_read_register(p,rd),0),0)); //V
+            flags += get_bit(overflowFromRev(shifter_operand - notcflag,rnVal,arm_read_register(p,rd),0),0); //V
             
             flags = flags<<28;                                                                  //shift de 28 
             
             arm_write_cpsr(p,flags|clearflagscpsr);
           }
+          return NO_ERROR;
         break;
 
         case TST:
@@ -323,19 +322,4 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
 
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
-}
-
-int and(arm_core p, int rd, int rn, int shifter_operand){
-    p->registers[rd]=p->registers[rn]&&shifter_operand;
-    return 0;
-}
-
-int add(arm_core p, int rd, int rn, int shifter_operand){
-    p->registers[rd]=p->registers[rn]+shifter_operand;
-    return 0;
-}
-
-int sub(arm_core p, int rd, int rn, int shifter_operand){
-    p->registers[rd]=p->registers[rn]-shifter_operand;
-    return 0;
 }
