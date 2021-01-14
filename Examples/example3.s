50586a71924f5c27dbd78159303fbfa603fb3d84
Pierrick Perelle <perellep@im2ag-mandelbrot.e.ujf-grenoble.fr>
2021-01-14 12:00:51 +0100

    exemple

--- Examples/example3.s
+++ Examples/example3.s
@@ -2,9 +2,15 @@
 .text
 main:
     ldr r0, =limite
-    ldrb r1, [r0]
-    add r0, r0, #3
-    ldrb r2, [r0]
+    ldr r1, [r0]
+    ldrb r2, [r0, #+2]
+    ldrb r2, [r0, #+3]
+    str r1, [r0, #+20]
+    str r1, [r0, #+16]
+    str r1, [r0, #+12]
+    str r1, [r0, #+8]
+    str r1, [r0, #+4]!
+    ldmIA r0, {r2, r3, r4, r5, r6}
     swi 0x123456
 .data
 limite:
